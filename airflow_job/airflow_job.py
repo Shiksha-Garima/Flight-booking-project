74f4d17eba88af58b6e39f9557bbf2a6057d3d4e
Shiksha-Garima <garima.doon2@gmail.com>
2026-03-21 00:24:52 +0530

    Start issue fixed again

--- airflow_job/airflow_job.py
+++ airflow_job/airflow_job.py
@@ -4,6 +4,7 @@ from airflow import DAG
 from airflow.providers.google.cloud.operators.dataproc import DataprocCreateBatchOperator
 from airflow.providers.google.cloud.sensors.gcs import GCSObjectExistenceSensor
 from airflow.models import Variable
+from airflow.utils.dates import days_ago
 
 # DAG default parameters
 default_args={
@@ -11,7 +12,7 @@ default_args={
     'depends_on_past':False,
     'retries':1,
     'retry_delay':timedelta(minutes=5),
-    'start_date':datetime(2026,3,21)
+    'start_date':days_ago(1),
 }
 
 # Define the DAG
